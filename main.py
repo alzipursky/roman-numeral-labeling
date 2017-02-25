818f0458862bd89c1e6804a380e5a09d59444adf
Aaron Zipursky <aaronzipursky@gmail.com>
2017-02-25 15:13:10 -0600

    loads csv and gets lowest pitch in segment

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-import runMelisma
+import runMelisma, os, pickle, csv
 
-out = runMelisma.getChordsAndPitches('melisma2003/midifiles/kp/tchaik.symph6.mid')
+labels_to_solfege = {}
+if os.path.isfile("labels_to_solfege.dat"):
+    f = open("labels_to_solfege.dat", "r")
+    u = pickle.Unpickler(f)
+    labels_to_solfege = u.load()
+    f.close()
+else:
+    with open("labels_to_solfege.csv", 'rb') as labelsToSolfegeCsv:
+        csvReader = csv.reader(labelsToSolfegeCsv, delimiter=',')
+        outputFile = open('labels_to_solfege.dat', 'wb')
+        for row in csvReader:
+            k = row[0]
+            v = []
+            for chroma in row[1:]:
+                v.append(chroma)
+            labels_to_solfege[k] = v
+        p = pickle.Pickler(outputFile)
+        p.dump(labels_to_solfege)
+        outputFile.close()
 
-for i in out:
-    print i
+print labels_to_solfege
+chordsAndPitches, lowestNotesInChord = runMelisma.getChordsAndPitches('melisma2003/midifiles/kp/tchaik.symph6.mid')
+
+for i in range(len(chordsAndPitches)):
+    print chordsAndPitches[i], lowestNotesInChord[i]
--- runMelisma.py
+++ runMelisma.py
@@ -1,21 +1,61 @@
 import os, time
 
+
 def getChordsAndPitches(midifile):
     os.system('melisma2003/mftextNEW/mftext ' + midifile + ' > new.notes')
     time.sleep(1)
     os.system('melisma2003/meter/meter new.notes | melisma2003/harmony/harmony > output.txt')
     time.sleep(1)
 
-    ignore = ['Beat','Finished','Chord','TPCNote','Analysis:']
+    ignore = ['Beat', 'Finished', 'Chord', 'TPCNote', 'Analysis:']
 
-    nums = [str(x) for x in range(9)]
+    nums = [str(x) for x in range(1,10)]
     lastChord = None
     chordsAndPitches = []
+
+    prevChord = None
+    chordsStartAndEnd = []
+    chordsStartAndEndDict = {}
+    dictNotInitialized = True
+    lowestNoteInChord = []
+
 
     with open('output.txt') as f:
         content = f.readlines()
         for row in content:
             chunks = row.split()
+            chunksC = row.split()
+            chunksN = row.split()
+
+            if len(chunksC) > 0 and chunksC[0] == 'Chord':
+                chunksC = chunksC[1:]
+
+                if prevChord is None or chunksC[2] != prevChord:
+                    chordsStartAndEnd.append((int(chunksC[0]),int(chunksC[1])))
+                    prevChord = chunksC[2]
+                else:
+                    tup = chordsStartAndEnd[-1]
+                    chordsStartAndEnd = chordsStartAndEnd[:len(chordsStartAndEnd)-1]
+                    newTup = (tup[0], int(chunksC[1]))
+                    chordsStartAndEnd.append(newTup)
+
+            if len(chunksN) > 0 and chunksN[0] == 'TPCNote':
+
+                if dictNotInitialized:
+                    for t in chordsStartAndEnd:
+                        chordsStartAndEndDict[t] = 99999
+                    dictNotInitialized = False
+
+                chunksN = chunksN[1:]
+
+                for t in chordsStartAndEnd:
+                    # if start time of note is at/after start of chord and end time of note is at/before end of chord
+                    # then it was played during the segment of that chord
+                    # check if it's the lowest midi value for that chord
+                    if int(chunksN[0]) >= t[0] and int(chunksN[1]) <= t[1]:
+                        if int(chunksN[2]) < chordsStartAndEndDict[t]:
+                            chordsStartAndEndDict[t] = int(chunksN[2])
+
             if len(chunks) > 0 and chunks[0] not in ignore:
                 chunks = [x.strip('>') for x in chunks]
                 chunks = [x for x in chunks if (x != '<' and x != '+' and x != 'x' and x != '|' and x != '' and x not in nums)]
@@ -24,9 +64,9 @@ def getChordsAndPitches(midifile):
                 # now first item is chord label, all items after that are pitches from that segment
                 if lastChord is None or chunks[0] != lastChord:
                     if len(chunks) > 1:
-                        chordsAndPitches.append((chunks[0],chunks[1:]))
+                        chordsAndPitches.append((chunks[0], chunks[1:]))
                     else:
-                        chordsAndPitches.append((chunks[0],[]))
+                        chordsAndPitches.append((chunks[0], []))
                     lastChord = chunks[0]
                 else:
                     if len(chunks) > 1:
@@ -34,4 +74,8 @@ def getChordsAndPitches(midifile):
                         chordsAndPitches = chordsAndPitches[:len(chordsAndPitches)-1]
                         newTup = (tup[0],tup[1] + chunks[1:])
                         chordsAndPitches.append(newTup)
-    return chordsAndPitches
+
+    for t in chordsStartAndEnd:
+        lowestNoteInChord.append(chordsStartAndEndDict[t])
+
+    return chordsAndPitches, lowestNoteInChord
