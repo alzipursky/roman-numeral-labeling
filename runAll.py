86569ead6d760053463e3f7f59f03786b3e7f8de
Aaron Zipursky <aaronzipursky@gmail.com>
2017-03-09 19:55:20 -0600

    bug in runall

--- runAll.py
+++ runAll.py
@@ -5,4 +5,4 @@ for filename in os.listdir('melisma2003/midifiles/kp'):
     if filename != 'README':
         print filename
         filename = 'melisma2003/midifiles/kp/' + filename
-        os.system('python milestone2Tmp2.py ' + filename)
+        os.system('python main.py ' + filename)
